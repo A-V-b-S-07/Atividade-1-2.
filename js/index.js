18cdda8b492d8724d40d4f0beed6e7efed3e2e96
AgnesSchmidt <126257362+A-V-b-S-07@users.noreply.github.com>
2023-11-16 18:09:24 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,57 @@
+
+
+
+/*
+    const nomePersonagem = "Vitória"
+
+    const sobrenome = "Schmidt"
+
+    const idade = 15
+
+    const empregado = false
+
+    console.log("Olá meu nome", nomePersonagem, "e meu sobrenome é", sobrenome, "tenho", idade, "anos. E estou desempregada", false)
+
+
+    
+    const nome = prompt("digite seu nome")
+    let idade = prompt("digite sua idade")
+
+    console.log(typeof nome)
+    console.log(typeof idade)
+
+
+    const numero = 123;
+    const numeroComoString = numero.toString();
+    console.log(typeof numeroComoString); // Saída: "string"
+
+    const stringNumerica = 456;
+    const stringConvertidaParaNumero = Number(stringNumerica);
+    console.log(typeof stringConvertidaParaNumero); // Saída: "number"*/
+
+
+
+    const nome = "Vitória"
+
+    const cidade = "SL"
+
+
+    console.log("Olá meu nome é", nome, "e minha cidade é", cidade)
+
+    console.log(typeof cidade)
+
+
+    const bolo = "sim"
+
+    const praia = "não"
+
+    const café = "sim"
+
+
+    console.log("Você gosta de bolo? ", bolo, ". Já foi na praia?", praia, ". E de café, você gosta?", café)
+
+    console.log(typeof bolo, praia, café)
+
+
+
+
